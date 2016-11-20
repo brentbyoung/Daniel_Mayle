c78a209caa60ee0585d382bcc188b28f84c74161
Daniel Mayle <dmayle012@gmail.com>
2016-11-20 14:08:37 -0800

    added subtract method to part I

--- Python_OOP/math_dojo.py
+++ Python_OOP/math_dojo.py
@@ -10,11 +10,15 @@ class MathDojo(object):
 		for x in numbers:
 			self.summed = self.summed + x
 		return self
+	def subtract(self, *numbers):
+		for x in numbers:
+			self.summed = self.summed - x
+		return self
 	def result(self):
 		print(self.summed)
 
 md = MathDojo()
-md.add(2).add(2, 5).subtract(3, 2).result()
+md.add(2).add(2, 5).subtract(2, 3).result()
 
 
 # PART II
