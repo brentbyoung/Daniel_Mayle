7f280101e88cb12b484c39d5e4801364b7912819
Daniel Mayle <dmayle012@gmail.com>
2016-11-27 13:50:36 -0800

    Time Display Assignment Added

---
+++ time_display_assignment/apps/timedisplay/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from  . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index') #views file show_users method
+    # url(r'^admin/', admin.site.urls),
+]
