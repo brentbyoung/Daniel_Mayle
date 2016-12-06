3919f3767494142cbe3fc36aa50ba9a02ac4d791
Daniel Mayle <dmayle012@gmail.com>
2016-12-06 14:09:34 -0800

    Django Courses Project Added

---
+++ courses_project/apps/courses_app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from models import Course
+
+def index(request):
+    context = {
+        'courses' : Course.objects.all()
+    }
+    return render(request, 'courses_app/index.html', context)
+
+def create(request):
+    Course.objects.create(name=request.POST['name'], description=request.POST['description'])
+    return redirect('/')
+
+def destroy(request, id):
+    course_to_delete = Course.objects.get(id=id)
+    if request.method == "GET":
+        return render(request, 'courses_app/confirm_delete.html', { 'course' : course_to_delete })
+
+    course_to_delete.delete()
+    return redirect('/')
